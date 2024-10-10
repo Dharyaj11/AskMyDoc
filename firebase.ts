a5ec0845e073b3281b8667474e184c2230eedb27
Dharyaj11 <dharyajasuja2003@gmail.com>
2024-10-10 23:59:58 +0530

    removed firbase

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,15 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCMmoGUDpMaYTXgXoDSwUUZ_pGbM4KOA8A",
+  authDomain: "paper-pal-f4b7e.firebaseapp.com",
+  projectId: "paper-pal-f4b7e",
+  storageBucket: "paper-pal-f4b7e.appspot.com",
+  messagingSenderId: "16270695990",
+  appId: "1:16270695990:web:db0d4e2ec38ffc6418c297",
+  measurementId: "G-LPJGMBYKGS"
+};
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
